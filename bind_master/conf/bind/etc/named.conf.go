96978265620b2e0f8aa1988c2745623b77218640
Caio Raveli <caio.raveli@gmail.com>
2020-12-15 00:10:16 -0300

    Bind VIEW - External and Internal

--- bind_master/conf/bind/etc/named.conf.go
+++ bind_master/conf/bind/etc/named.conf.go
@@ -1,1 +1,18 @@
-include "/etc/bind/zones/go/internal/go.com.br.conf";
+//include "/etc/bind/zones/go/internal/go.com.br.conf";
+view "internal-view" {
+	match-clients { net_85; };
+	include "/etc/bind/zones/go/internal/go.com.br.conf";
+	include "/etc/bind/zones.rfc1918";
+	include "/etc/bind/named.conf.default-zones";
+};
+
+view "external-view" {
+//	include "/etc/bind/zones/go/external/go.com.br.conf";
+	match-clients { any; };
+	recursion no;
+	include "/etc/bind/zones/go/external/go.com.br.conf";
+};
+
+acl net_85 {
+	10.85.0.20/32;
+};
